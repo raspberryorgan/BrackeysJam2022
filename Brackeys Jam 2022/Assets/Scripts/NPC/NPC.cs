ca392e06804857608e0717722c7334c1cdeca0a7
Belen Nava <raspberryorgan@gmail.com>
2022-02-22 23:12:26 -0300

    NPC Clay

--- Brackeys Jam 2022/Assets/Scripts/NPC/NPC.cs
+++ Brackeys Jam 2022/Assets/Scripts/NPC/NPC.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NPC : Interactable
 {
     public Mission mission;
+    public Player player;
     // Start is called before the first frame update
     void Start()
     {
