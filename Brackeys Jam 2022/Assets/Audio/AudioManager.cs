b31bf54ef6b8b9469879787585c9a037d18f3977
Belen Nava <raspberryorgan@gmail.com>
2022-02-24 20:05:42 -0300

    Cloud + Audio Manager

---
+++ Brackeys Jam 2022/Assets/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    void Awake() {
+        if ( instance != null ) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        foreach(Sound s in sounds)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.clip;
+            s.audioSource.volume = s.volume;
+            s.audioSource.pitch = s.pitch;
+            s.audioSource.loop = s.loop;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.X))
+        {
+            Play("test");
+        }
+	}
+    public void Play(string soundName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == soundName);
+        if (s!= null)
+        {
+            s.audioSource.Play();
+        }
+    }
+}
---
+++ Brackeys Jam 2022/Assets/Audio/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound  {
+    public AudioClip clip;
+    public string name;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0f, 1f)]
+    public float volumeVariance = .1f;
+
+
+    [Range(.1f, 3f)]
+    public float pitch;
+    [Range(0f, 1f)]
+    public float pitchVariance = .1f;
+
+    public bool loop;
+    [HideInInspector]
+    public AudioSource audioSource;
+}
